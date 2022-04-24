2a59669a205f558bb750921bc7c75b448f94f6b0
dlddmddl <103375409+dlddmddl@users.noreply.github.com>
2022-04-25 02:15:43 +0900

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,25 @@
+for i in range(1,26,5):
+     for j in range(1,16,3) :
+         for k in range(1,11,2):
+             if (i+j+k==23):
+                 print("5g추 %d개 3g추 %d개 2g추 %d개" %(i/5,j/3,k/2))         
+             if (i+j+k==24) :      
+                 print("5g추 %d개 3g추 %d개 2g추 %d개\n" %(i/5,j/3,k/2))
+             if (i+j+k==25) :
+                 print("5g추 %d개 3g추 %d개 2g추 %d개\n" %(i/5,j/3,k/2))
+   
+                 
+                 
+            
+             
+   
+             
+               
+            
+
+        
+
+    
+
+    
+    
